0a254f36080111af0616f7c46e98acf1311be194
Syohei YOSHIDA <syohex@gmail.com>
2014-04-10 21:08:58 +0900

    add LinkButton example

---
+++ r8/linkbutton_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+
+from gi.repository import Gtk
+
+class LinkButtonWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="LinkButton Demo")
+        self.set_border_width(10)
+
+        button = Gtk.LinkButton("http://www.gtk.org", "Visit Gtk+ Homepage")
+        self.add(button)
+
+win = LinkButtonWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
